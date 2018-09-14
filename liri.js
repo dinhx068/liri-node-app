ef8fe1c934a7d4a7039c890ec7bc1e24b61fbde1
Joe <dinhx068@umn.edu>
2018-09-14 12:57:22 -0500

    formatted date/time and using lowercase() for input

--- liri.js
+++ liri.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 
 var fs = require('fs');
+// Did this so user input does not fail on uppercase
+var lowerCase = require('lower-case')
 
-var command = process.argv[2];
-var search = process.argv[3];
+var command = lowerCase(process.argv[2]);
+var search = lowerCase(process.argv[3]);
 
 switch (command) {
     case 'concert-this':
@@ -39,12 +41,45 @@ switch (command) {
         addToLog(command);
 }
 
+/** https://gist.github.com/hurjas/2660489
+ * Return a timestamp with the format "m/d/yy h:MM:ss TT"
+ * @type {Date}
+ */
+
+function timeStamp() {
+// Create a date object with the current time
+    let now = new Date();
+
+// Create an array with the current month, day and time
+    let date = [now.getMonth() + 1, now.getDate(), now.getFullYear()];
+
+// Create an array with the current hour, minute and second
+    let time = [now.getHours(), now.getMinutes(), now.getSeconds()];
+
+// Determine AM or PM suffix based on the hour
+    let suffix = (time[0] < 12) ? "AM" : "PM";
+
+// Convert hour from military time
+    time[0] = (time[0] < 12) ? time[0] : time[0] - 12;
+
+// If hour is 0, set it to 12
+    time[0] = time[0] || 12;
+
+// If seconds and minutes are less than 10, add a zero
+    for (let i = 1; i < 3; i++) {
+        if (time[i] < 10) {
+            time[i] = "0" + time[i];
+        }
+    }
+
+// Return the formatted string
+    return date.join("/") + " " + time.join(":") + " " + suffix;
+}
+
 // Writing to the log file on every command
 function addToLog(command) {
-    var today = new Date();
-    var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
-    var date = today.getFullYear() + '-'+ today.getMonth() + '-' + today.getDate();
-    fs.appendFile('log.txt', `${date} ${time} | Command: ${command} \r\n`, function(err) {
+    let formatted = timeStamp();
+    fs.appendFile('log.txt', `${formatted} | Command: ${command} \r\n`, function(err) {
         if (err) {
             return console.log(err);
         }
